35d2af551ae7190247324504b74d3009f92b10db
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-03-18 12:39:08 -0300

    Requisito 4
        Cria um endpoint "/talker" com o metodo "post" e faz a verificação do "authorization" presente no "header" adquirido através do login, verifica também o "name" , "age", e "talk" que possui "watchedAt" e "rate" enviados através do "body", caso alguma verificação não esteja de acordo será retornado um erro, caso esteja tudo correto retornará os dados da pessoa palestrante que foi cadastrada.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const crypto = require('crypto');
 const loginController = require('./controllers/loginController');
+const talkerController = require('./controllers/talkerController');
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,12 +20,12 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (_req, res) => fs.readFile(apiTalker, 'utf8')
   .then((data) => res.status(200).json(JSON.parse(data)))
-    .catch((_err) => console.log('erro')));
+  .catch((_err) => console.log('erro')));
 
 app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
 
-  fs.readFile(apiTalker, 'utf8')
+  await fs.readFile(apiTalker, 'utf8')
     .then((data) => {
       if (JSON.parse(data).find((item) => item.id === Number(id))) {
         return res.status(200).json(JSON.parse(data).find((item) => item.id === Number(id)));
@@ -37,6 +38,23 @@ app.get('/talker/:id', async (req, res) => {
 app.post('/login', loginController.email, loginController.password, (_req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   return res.status(200).json({ token });
+});
+
+app.post('/talker', talkerController.talkerControllerArr, async (req, res) => {
+  const { name, age, talk: { watchedAt, rate } } = req.body;
+  fs.readFile('./talker.json', 'utf8')
+    .then(async (data) => {
+      const dataJson = JSON.parse(data);
+      const newId = dataJson[dataJson.length - 1].id + 1;
+      dataJson.push({ name, age, id: newId, talk: { watchedAt, rate } });
+      try {
+        await fs.writeFile('./talker.json', JSON.stringify(dataJson));
+        res.status(201).json({ name, age, id: newId, talk: { watchedAt, rate } });
+      } catch (err) {
+        console.log(err);
+      }
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => {
