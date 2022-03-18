4d433140802c2de9eebc1b9d7a8d1a69f3fac302
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-03-17 23:09:57 -0300

    Requisito 2
    Cria um endpoint "talker/:id"  que lê o arquivo "talker.json" com "fs" presente no node e retorna um filtro com o "id" resgatado através do params. Caso o "id" não seja encontrado retorna um "status 404"

--- index.js
+++ index.js
@@ -7,15 +7,29 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const apiTalker = './talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (_req, res) => fs.readFile('./talker.json', 'utf8')
+app.get('/talker', async (_req, res) => fs.readFile(apiTalker, 'utf8')
     .then((data) => res.status(200).json(JSON.parse(data)))
-    .catch((err) => console.log('erro', err)));
+    .catch((_err) => console.log('erro')));
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+
+  fs.readFile(apiTalker, 'utf8')
+  .then((data) => {
+    if (JSON.parse(data).find((item) => item.id === Number(id))) {
+      return res.status(200).json(JSON.parse(data).find((item) => item.id === Number(id)));
+    }
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  })
+  .catch((_err) => console.log('erro'));
+});
 
 app.listen(PORT, () => {
   console.log('Online');
